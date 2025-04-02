019fde57ac916d69b2aaa2c0d5c4449ecd66f604
Okuniewska, Julia <julia.okuniewska@intel.com>
2025-04-02 09:34:36 +0000

    fix: docker infra provider version in vendoring

--- internal/controller/suite_test.go
+++ internal/controller/suite_test.go
@@ -100,7 +100,7 @@ var _ = BeforeSuite(func() {
 			filepath.Join(build.Default.GOPATH, "pkg", "mod", "github.com", "rancher", "cluster-api-provider-rke2@v0.12.0", "controlplane", "config", "crd", "bases"),
 			filepath.Join(build.Default.GOPATH, "pkg", "mod", "sigs.k8s.io", "cluster-api@v1.9.5", "controlplane", "kubeadm", "config", "crd", "bases"),
 			filepath.Join(build.Default.GOPATH, "pkg", "mod", "sigs.k8s.io", "cluster-api@v1.9.5", "config", "crd", "bases"),
-			filepath.Join(build.Default.GOPATH, "pkg", "mod", "sigs.k8s.io", "cluster-api", "test@v1.8.5", "infrastructure", "docker", "config", "crd", "bases"),
+			filepath.Join(build.Default.GOPATH, "pkg", "mod", "sigs.k8s.io", "cluster-api", "test@v1.9.5", "infrastructure", "docker", "config", "crd", "bases"),
 			filepath.Join(build.Default.GOPATH, "pkg", "mod", "github.com", "open-edge-platform", "cluster-api-provider-intel@v1.0.1", "config", "crd", "bases"),
 		},
 		ErrorIfCRDPathMissing: true,
