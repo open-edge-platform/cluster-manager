ce7dea883c808062ad3709e69e870dcdeefc67da
Okuniewska, Julia <julia.okuniewska@intel.com>
2025-04-22 09:42:12 +0000

    test: add reconciliation tests for cluster template with kubeadm + intel

--- internal/controller/clustertemplate_controller_test.go
+++ internal/controller/clustertemplate_controller_test.go
@@ -363,3 +363,113 @@ var _ = Describe("ClusterTemplate Controller for RKE2 CP and Intel Infra", func(
 		})
 	})
 })
+
+var _ = Describe("ClusterTemplate Controller for RKE2 CP and Intel Infra", func() {
+	Context("When reconciling a resource", func() {
+		const resourceName = "test-resource"
+
+		ctx := context.Background()
+
+		typeNamespacedName := types.NamespacedName{
+			Name:      resourceName,
+			Namespace: "default", // TODO(user):Modify as needed
+		}
+		clustertemplate := &clusterv1alpha1.ClusterTemplate{}
+
+		BeforeEach(func() {
+			By("creating the custom resource for the Kind ClusterTemplate")
+			err := k8sClient.Get(ctx, typeNamespacedName, clustertemplate)
+			if err != nil && errors.IsNotFound(err) {
+				resource := &clusterv1alpha1.ClusterTemplate{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      resourceName,
+						Namespace: "default",
+					},
+					Spec: clusterv1alpha1.ClusterTemplateSpec{
+						ControlPlaneProviderType: "kubeadm",
+						InfraProviderType:        "intel",
+						KubernetesVersion:        "v1.30.10",
+						ClusterConfiguration:     "{\"apiVersion\":\"controlplane.cluster.x-k8s.io/v1beta1\",\"kind\":\"KubeadmControlPlaneTemplate\",\"metadata\":{\"name\":\"kubeadm-control-plane-template-v0.1.0\"},\"spec\":{\"template\":{\"spec\":{\"kubeadmConfigSpec\":{\"clusterConfiguration\":{\"apiServer\":{\"certSANs\":[\"localhost\",\"127.0.0.1\",\"0.0.0.0\",\"host.docker.internal\"]}},\"initConfiguration\":{\"nodeRegistration\":{}},\"joinConfiguration\":{\"nodeRegistration\":{}},\"postKubeadmCommands\":[\"kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/calico.yaml\"]}}}}}",
+						ClusterNetwork: clusterv1alpha1.ClusterNetwork{
+							Services: &clusterv1alpha1.NetworkRanges{
+								CIDRBlocks: []string{"10.43.0.0/16"},
+							},
+							Pods: &clusterv1alpha1.NetworkRanges{
+								CIDRBlocks: []string{"10.42.0.0/16"},
+							},
+						},
+						ClusterLabels: map[string]string{
+							"default-extension": "privileged",
+						},
+					},
+				}
+				Expect(k8sClient.Create(ctx, resource)).To(Succeed())
+			}
+		})
+
+		AfterEach(func() {
+			// TODO(user): Cleanup logic after each test, like removing the resource instance.
+			resource := &clusterv1alpha1.ClusterTemplate{}
+			err := k8sClient.Get(ctx, typeNamespacedName, resource)
+			Expect(err).NotTo(HaveOccurred())
+
+			By("validating the finalizer is present")
+			Expect(controllerutil.ContainsFinalizer(resource, clusterv1alpha1.ClusterTemplateFinalizer)).To(BeTrue())
+
+			By("Cleanup the specific resource instance ClusterTemplate")
+			Expect(k8sClient.Delete(ctx, resource)).To(Succeed())
+
+			By("Reconciling the deleted resource")
+			controllerReconciler := &ClusterTemplateReconciler{
+				Client: k8sClient,
+				Scheme: k8sClient.Scheme(),
+			}
+
+			_, err = controllerReconciler.Reconcile(ctx, reconcile.Request{
+				NamespacedName: typeNamespacedName,
+			})
+			Expect(err).NotTo(HaveOccurred())
+
+			By("validating the finalizer is removed")
+			err = k8sClient.Get(ctx, typeNamespacedName, resource)
+			if err == nil {
+				Expect(controllerutil.ContainsFinalizer(resource, clusterv1alpha1.ClusterTemplateFinalizer)).To(BeFalse())
+			}
+		})
+
+		It("should successfully reconcile the resource", func() {
+			By("Reconciling the created resource")
+			controllerReconciler := &ClusterTemplateReconciler{
+				Client: k8sClient,
+				Scheme: k8sClient.Scheme(),
+			}
+
+			_, err := controllerReconciler.Reconcile(ctx, reconcile.Request{
+				NamespacedName: typeNamespacedName,
+			})
+			Expect(err).NotTo(HaveOccurred())
+
+			Expect(k8sClient.Get(ctx, typeNamespacedName, clustertemplate)).To(Succeed())
+			Expect(clustertemplate.Status.Ready).To(BeTrue())
+
+			By("validating the IntelMachineTemplate is created")
+			err = k8sClient.Get(ctx, types.NamespacedName{
+				Name:      fmt.Sprintf("%s-controlplane", typeNamespacedName.Name),
+				Namespace: typeNamespacedName.Namespace,
+			}, &intelv1alpha1.IntelMachineTemplate{})
+			Expect(err).NotTo(HaveOccurred())
+
+			By("validating the IntelClusterTemplate is created")
+			err = k8sClient.Get(ctx, typeNamespacedName, &intelv1alpha1.IntelClusterTemplate{})
+			Expect(err).NotTo(HaveOccurred())
+
+			By("validating the KubeadmControlPlaneTemplate is created")
+			err = k8sClient.Get(ctx, typeNamespacedName, &kubeadmcpv1beta1.KubeadmControlPlaneTemplate{})
+			Expect(err).NotTo(HaveOccurred())
+
+			By("validating the ClusterClass is created")
+			err = k8sClient.Get(ctx, typeNamespacedName, &capiv1beta1.ClusterClass{})
+			Expect(err).NotTo(HaveOccurred())
+		})
+	})
+})
