be7fd56e480645ce015d6b6c7672e3cfba35fd37
Gowdra, Girish <girish.gowdra@intel.com>
2025-05-22 13:07:20 -0700

    feat: support filtering on k8s version

--- internal/rest/getv2templates_test.go
+++ internal/rest/getv2templates_test.go
@@ -47,28 +47,6 @@ var templateInfo1 = api.TemplateInfo{
 	},
 }
 
-var templateInfo2 = api.TemplateInfo{
-	Name:                     "test-template",
-	Version:                  "v0.0.2",
-	Controlplaneprovidertype: (*api.TemplateInfoControlplaneprovidertype)(ptr("kubeadm")),
-	Infraprovidertype:        (*api.TemplateInfoInfraprovidertype)(ptr("docker")),
-	KubernetesVersion:        "1.21.0",
-	Clusterconfiguration: &map[string]interface{}{
-		"fake": "config",
-	},
-}
-
-var templateInfo3 = api.TemplateInfo{
-	Name:                     "test-other-template",
-	Version:                  "v0.0.2",
-	Controlplaneprovidertype: (*api.TemplateInfoControlplaneprovidertype)(ptr("kubeadm")),
-	Infraprovidertype:        (*api.TemplateInfoInfraprovidertype)(ptr("docker")),
-	KubernetesVersion:        "1.21.0",
-	Clusterconfiguration: &map[string]interface{}{
-		"fake": "config",
-	},
-}
-
 var template2 = v1alpha1.ClusterTemplate{
 	ObjectMeta: v1.ObjectMeta{
 		Name: "test-template-v0.0.2",
@@ -76,7 +54,18 @@ var template2 = v1alpha1.ClusterTemplate{
 	Spec: v1alpha1.ClusterTemplateSpec{
 		ControlPlaneProviderType: "kubeadm",
 		InfraProviderType:        "docker",
-		KubernetesVersion:        "1.21.0",
+		KubernetesVersion:        "1.33.0",
+	},
+}
+
+var templateInfo2 = api.TemplateInfo{
+	Name:                     "test-template",
+	Version:                  "v0.0.2",
+	Controlplaneprovidertype: (*api.TemplateInfoControlplaneprovidertype)(ptr("kubeadm")),
+	Infraprovidertype:        (*api.TemplateInfoInfraprovidertype)(ptr("docker")),
+	KubernetesVersion:        "1.33.0",
+	Clusterconfiguration: &map[string]interface{}{
+		"fake": "config",
 	},
 }
 
@@ -87,7 +76,40 @@ var template3 = v1alpha1.ClusterTemplate{
 	Spec: v1alpha1.ClusterTemplateSpec{
 		ControlPlaneProviderType: "kubeadm",
 		InfraProviderType:        "docker",
-		KubernetesVersion:        "1.21.0",
+		KubernetesVersion:        "1.33.0",
+	},
+}
+
+var templateInfo3 = api.TemplateInfo{
+	Name:                     "test-other-template",
+	Version:                  "v0.0.2",
+	Controlplaneprovidertype: (*api.TemplateInfoControlplaneprovidertype)(ptr("kubeadm")),
+	Infraprovidertype:        (*api.TemplateInfoInfraprovidertype)(ptr("docker")),
+	KubernetesVersion:        "1.33.0",
+	Clusterconfiguration: &map[string]interface{}{
+		"fake": "config",
+	},
+}
+
+var template4 = v1alpha1.ClusterTemplate{
+	ObjectMeta: v1.ObjectMeta{
+		Name: "test-other-template-v0.0.3",
+	},
+	Spec: v1alpha1.ClusterTemplateSpec{
+		ControlPlaneProviderType: "kubeadm",
+		InfraProviderType:        "docker",
+		KubernetesVersion:        "1.33.0",
+	},
+}
+
+var templateInfo4 = api.TemplateInfo{
+	Name:                     "test-other-template",
+	Version:                  "v0.0.3",
+	Controlplaneprovidertype: (*api.TemplateInfoControlplaneprovidertype)(ptr("kubeadm")),
+	Infraprovidertype:        (*api.TemplateInfoInfraprovidertype)(ptr("docker")),
+	KubernetesVersion:        "1.33.0",
+	Clusterconfiguration: &map[string]interface{}{
+		"fake": "config",
 	},
 }
 
@@ -270,6 +292,87 @@ func TestGetV2Templates200(t *testing.T) {
 		require.Equal(t, http.StatusOK, rr.Code, "ServeHTTP() status = %v, want %v", rr.Code, 200)
 		require.Len(t, *response.TemplateInfoList, 1, "TemplateInfoList should have 1 item")
 		require.Equal(t, templateInfo3.Name, (*response.TemplateInfoList)[0].Name, "TemplateInfoList[0].Name = %v, want %v", (*response.TemplateInfoList)[0].Name, template3.Name)
+		require.Empty(t, response.DefaultTemplateInfo, "DefaultTemplateInfo should be empty")
+	})
+
+	t.Run("Three templates - filter k8s version", func(t *testing.T) {
+		server := createMockServerTemplates(t, []v1alpha1.ClusterTemplate{template1, template2, template3}, expectedActiveProjectID, nil)
+		require.NotNil(t, server, "NewServer() returned nil, want not nil")
+
+		// Create a new request & response recorder
+		req := httptest.NewRequest("GET", "/v2/templates?filter=kubernetesVersion=1.33.0", nil)
+		req.Header.Set("Activeprojectid", expectedActiveProjectID)
+		rr := httptest.NewRecorder()
+
+		// create a handler with middleware to serve the request
+		handler, err := server.ConfigureHandler()
+		require.Nil(t, err)
+		handler.ServeHTTP(rr, req)
+
+		// Parse the response body
+		var response api.GetV2Templates200JSONResponse
+		err = json.Unmarshal(rr.Body.Bytes(), &response)
+		require.NoError(t, err, "Failed to unmarshal response body")
+
+		// Check the response status
+		require.Equal(t, http.StatusOK, rr.Code, "ServeHTTP() status = %v, want %v", rr.Code, 200)
+		require.Len(t, *response.TemplateInfoList, 2, "TemplateInfoList should have 2 items")
+		require.Equal(t, templateInfo2.Name, (*response.TemplateInfoList)[0].Name, "TemplateInfoList[0].Name = %v, want %v", (*response.TemplateInfoList)[0].Name, template2.Name)
+		require.Equal(t, templateInfo3.Name, (*response.TemplateInfoList)[1].Name, "TemplateInfoList[1].Name = %v, want %v", (*response.TemplateInfoList)[1].Name, template3.Name)
+		require.Empty(t, response.DefaultTemplateInfo, "DefaultTemplateInfo should be empty")
+	})
+
+	t.Run("Three templates - filter k8s version - partial search", func(t *testing.T) {
+		server := createMockServerTemplates(t, []v1alpha1.ClusterTemplate{template1, template2, template3}, expectedActiveProjectID, nil)
+		require.NotNil(t, server, "NewServer() returned nil, want not nil")
+
+		// Create a new request & response recorder
+		req := httptest.NewRequest("GET", "/v2/templates?filter=kubernetesVersion=1.33", nil)
+		req.Header.Set("Activeprojectid", expectedActiveProjectID)
+		rr := httptest.NewRecorder()
+
+		// create a handler with middleware to serve the request
+		handler, err := server.ConfigureHandler()
+		require.Nil(t, err)
+		handler.ServeHTTP(rr, req)
+
+		// Parse the response body
+		var response api.GetV2Templates200JSONResponse
+		err = json.Unmarshal(rr.Body.Bytes(), &response)
+		require.NoError(t, err, "Failed to unmarshal response body")
+
+		// Check the response status
+		require.Equal(t, http.StatusOK, rr.Code, "ServeHTTP() status = %v, want %v", rr.Code, 200)
+		require.Len(t, *response.TemplateInfoList, 2, "TemplateInfoList should have 2 items")
+		require.Equal(t, templateInfo2.Name, (*response.TemplateInfoList)[0].Name, "TemplateInfoList[0].Name = %v, want %v", (*response.TemplateInfoList)[0].Name, template2.Name)
+		require.Equal(t, templateInfo3.Name, (*response.TemplateInfoList)[1].Name, "TemplateInfoList[1].Name = %v, want %v", (*response.TemplateInfoList)[1].Name, template3.Name)
+		require.Empty(t, response.DefaultTemplateInfo, "DefaultTemplateInfo should be empty")
+	})
+
+	t.Run("Four templates - filter k8s version and order by template version", func(t *testing.T) {
+		server := createMockServerTemplates(t, []v1alpha1.ClusterTemplate{template1, template2, template3, template4}, expectedActiveProjectID, nil)
+		require.NotNil(t, server, "NewServer() returned nil, want not nil")
+
+		// Create a new request & response recorder
+		req := httptest.NewRequest("GET", "/v2/templates?filter=kubernetesVersion=1.33.0%20AND%20name=test-other-template&orderBy=version", nil)
+		req.Header.Set("Activeprojectid", expectedActiveProjectID)
+		rr := httptest.NewRecorder()
+
+		// create a handler with middleware to serve the request
+		handler, err := server.ConfigureHandler()
+		require.Nil(t, err)
+		handler.ServeHTTP(rr, req)
+
+		// Parse the response body
+		var response api.GetV2Templates200JSONResponse
+		err = json.Unmarshal(rr.Body.Bytes(), &response)
+		require.NoError(t, err, "Failed to unmarshal response body")
+
+		// Check the response status
+		require.Equal(t, http.StatusOK, rr.Code, "ServeHTTP() status = %v, want %v", rr.Code, 200)
+		require.Len(t, *response.TemplateInfoList, 2, "TemplateInfoList should have 2 items")
+		require.Equal(t, templateInfo3.Name, (*response.TemplateInfoList)[0].Name, "TemplateInfoList[1].Name = %v, want %v", (*response.TemplateInfoList)[0].Name, template3.Name)
+		require.Equal(t, templateInfo4.Name, (*response.TemplateInfoList)[1].Name, "TemplateInfoList[2].Name = %v, want %v", (*response.TemplateInfoList)[1].Name, template4.Name)
 		require.Empty(t, response.DefaultTemplateInfo, "DefaultTemplateInfo should be empty")
 	})
 
