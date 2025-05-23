e65f5418e125b6ca7435ae03ba68d98d5b33d899
Gowdra, Girish <girish.gowdra@intel.com>
2025-05-23 15:51:01 -0700

    fix: lint issues

--- internal/providers/k3sintel.go
+++ internal/providers/k3sintel.go
@@ -7,6 +7,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+
 	corev1 "k8s.io/api/core/v1"
 	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
