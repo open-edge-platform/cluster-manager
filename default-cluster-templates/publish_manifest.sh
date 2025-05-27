f910a6479cdb04cedbafbb9c26d19de6afb8f9c4
Lazar, Madalina <madalina.lazar@intel.com>
2025-05-27 15:23:23 +0000

    test: fix backslash encoding and interpretation issues
    Signed-off-by: Lazar, Madalina <madalina.lazar@intel.com>

--- default-cluster-templates/publish_manifest.sh
+++ default-cluster-templates/publish_manifest.sh
@@ -8,15 +8,24 @@ echo $LPP_MANIFEST_PATH
 test_var=$(jq -Rs '.' < $LPP_MANIFEST_PATH)
 
 # Double escape the JSON string
-DOUBLE_ESCAPED=$(echo "$test_var" | sed 's/\\/\\\\/g; s/\n/\\n/g')
-QUOTE_ESCAPED=$(echo "$DOUBLE_ESCAPED" | sed 's/^"//; s/"$//')
-# Verify the double-escaped content
-echo "quote-escaped content: $QUOTE_ESCAPED"
-# echo "test var: $(test_var)"
-# sed -i.bak 's@__MANIFEST_PLACEHOLDER__@'"$(printf '%s\n' "$(test_var)")"'@' baseline.json
-# sed -i.bak 's@__MANIFEST_PLACEHOLDER__@'"$(printf '%s\n' "$test_var")"'@' $BASELINE_TEMPLATE
+# DOUBLE_ESCAPED=$(echo "$test_var" | sed 's/\\/\\\\/g; s/\n/\\n/g')
+# QUOTE_ESCAPED=$(echo "$DOUBLE_ESCAPED" | sed 's/^"//; s/"$//')
+# TRIPLE_ESCAPED=$(echo "$QUOTE_ESCAPED" | sed 's/"/\\\\\\\\\\\\\\"/g')
+# # Verify the double-escaped content
+# echo "quote-escaped content: $TRIPLE_ESCAPED"
+# # echo "test var: $(test_var)"
+# # sed -i.bak 's@__MANIFEST_PLACEHOLDER__@'"$(printf '%s\n' "$(test_var)")"'@' baseline.json
+# # sed -i.bak 's@__MANIFEST_PLACEHOLDER__@'"$(printf '%s\n' "$test_var")"'@' $BASELINE_TEMPLATE
 
-awk -v placeholder="$LPP_PLACEHOLDER" -v replacement="$QUOTE_ESCAPED" '{gsub(placeholder, replacement); print}' "$BASELINE_TEMPLATE" > "$BASELINE_TEMPLATE.tmp" && mv "$BASELINE_TEMPLATE.tmp" "$BASELINE_TEMPLATE"
+# awk -v placeholder="$LPP_PLACEHOLDER" -v replacement="$TRIPLE_ESCAPED" '{gsub(placeholder, replacement); print}' "$BASELINE_TEMPLATE" > "$BASELINE_TEMPLATE.tmp" && mv "$BASELINE_TEMPLATE.tmp" "$BASELINE_TEMPLATE"
 
 
-echo "test var end"
+# echo "test var end"
+
+# second iteration
+CONTENT_STRIPPED=${test_var:1:-1}
+ESCAPED_FOR_JSON=$(echo "$CONTENT_STRIPPED" | sed -e 's/\\/\\\\\\\\/g' -e 's/"/\\\\\\"/g')
+awk -v placeholder="$LPP_PLACEHOLDER" -v replacement="$ESCAPED_FOR_JSON" '{
+    gsub(placeholder, replacement);
+    print
+}' "$BASELINE_TEMPLATE" > "$BASELINE_TEMPLATE.tmp" && mv "$BASELINE_TEMPLATE.tmp" "$BASELINE_TEMPLATE"
