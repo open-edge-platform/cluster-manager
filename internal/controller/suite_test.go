79868c25e19677ff2f5c90082716edcbedc2c1a7
togashidm <denisio.togashi@intel.com>
2025-11-06 11:33:45 +0000

    chore: remove debug patch

--- internal/controller/suite_test.go
+++ internal/controller/suite_test.go
@@ -163,14 +163,8 @@ var _ = BeforeSuite(func() {
 		// default path defined in controller-runtime which is /usr/local/kubebuilder/.
 		// Note that you must have the required binaries setup under the bin directory to perform
 		// the tests directly. When we run make test it will be setup and used automatically.
-	}
-
-	// Use KUBEBUILDER_ASSETS if set (from make test), otherwise use relative path
-	if binPath := os.Getenv("KUBEBUILDER_ASSETS"); binPath != "" {
-		testEnv.BinaryAssetsDirectory = binPath
-	} else {
-		testEnv.BinaryAssetsDirectory = filepath.Join("..", "..", "bin", "k8s",
-			fmt.Sprintf("1.31.0-%s-%s", runtime.GOOS, runtime.GOARCH))
+		BinaryAssetsDirectory: filepath.Join("..", "..", "bin", "k8s",
+			fmt.Sprintf("1.31.0-%s-%s", runtime.GOOS, runtime.GOARCH)),
 	}
 
 	// cfg is defined in this file globally.
